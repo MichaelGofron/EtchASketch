a87275748c19d1b148ff7bc20b1840303af23f90
Michael Gofron <gofronm@yahoo.com>
2014-12-25 18:21:48 -0500

    Etch A Sketch Start

---
+++ EtchASketch/public_html/functions.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+    for (var row = 0; row < 16; row++){
+        for (var col = 0; col < 16; col++){
+            
+        }
+    }
+})
+
